5362a04df01fff18f201e797c7cb37022becbeab
Michael Peterson <WebDev@ctoms.ca>
2019-03-11 14:08:43 -0600

    add customer tag check

--- rcmpDiscountClass.rb
+++ rcmpDiscountClass.rb
@@ -6,22 +6,23 @@ class RCMPDiscounts
 	end
 
 	def run()
-		@cart.line_items.each do |line_item|
-			product = line_item.variant.product
-			currentProductTags = product&.tags
-			currentProductTags.each do |tag|
-				if tag.include?(@discountTag)
-					quantity = line_item.quantity
-					trimmedTag = tag.split("__")[1]				
-					discountPrice = trimmedTag.split("_")[1]
-					newPrice = Money.new(cents: 100) * discountPrice * quantity
-					line_item.change_line_price(newPrice, message: @message)
-					
-				end
-			end		
-		end		
-	end
-	
+		if @cart.customer&.tags&.any? {|tag| tag.downcase.include?(@discountTag)}
+			@cart.line_items.each do |line_item|
+				product = line_item.variant.product
+				currentProductTags = product&.tags
+				currentProductTags.each do |tag|
+					if tag.include?(@discountTag)
+						quantity = line_item.quantity
+						trimmedTag = tag.split("__")[1]				
+						discountPrice = trimmedTag.split("_")[1]
+						newPrice = Money.new(cents: 100) * discountPrice * quantity
+						line_item.change_line_price(newPrice, message: @message)
+						
+					end
+				end		
+			end	
+		end	
+	end	
 end
 
 CAMPAIGNS = [RCMPDiscounts.new(Input.cart)]
